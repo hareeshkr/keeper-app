6a317e39c8bd761e3c1e43e489d647f1f5d434e6
hareeshkr <hareeshkr@pm.me>
2021-01-05 18:50:59 +0530

    first version

---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note(){
+    return (<div className="note">
+        <h1>My Note</h1>
+        <p>This is my first note.</p>
+    </div>);
+
+}
+
+export default Note;
